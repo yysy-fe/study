d06d4dc62ec4b3cc163f2197f839fd43cdfc8b10
syshenyang <468068459@qq.com>
2019-09-18 11:24:42 +0800

    完善bind如果没传参数时的问题

--- code/bindCallApply/bind.js
+++ code/bindCallApply/bind.js
@@ -1,5 +1,11 @@
 Function.prototype.myBind = function (context, ...args) {
   return (...args2) => {
-    this.apply(context, [...args, ...args2]);
+    let param = [...args, ...args2];
+    if (param.length === 0) {
+      return this.apply(context);
+    } else {
+      return this.apply(context, param);
+    }
+    
   }
 }
